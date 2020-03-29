e52fbfc66958776940260021a9d68c04a81a876a
mariaviviana <elmaiten2@gmail.com>
2020-03-29 10:01:28 -0300

    Agregue FOR

--- ejercicio.js
+++ ejercicio.js
@@ -1,5 +1,9 @@
 console.log ("Buenas noches");
 console.log("Que tu vida sea larga y util como el papel higienico");
-let numeros = [1,2,3,4];
-console.log(numeros);
 
+let numeros = [1,2,3,4];
+function verArray(numeros){
+    for(let i=0; i<numeros.length; i++){
+    console.log(numeros[i]);
+   };
+}
